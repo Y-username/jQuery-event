99a50945a1c9f19d892742ff135759e6a8ca0f08
Yolanda Han <han.yolanda@icloud.com>
2019-03-22 15:06:18 +0000

    edited jquery again

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
+/*global $*/
+
 $(document).ready(function() {
-	$("#stream1_btn").on("click", function() {
-		$(".stream1").fadeTo(2000, 0.5);
-		$(".stream1").fadeOut(1000);
-		$(".stream1").fadeIn();
-	});
-	$("#stream2_btn").mouseenter("click", function() {
-		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream2").addClass('highlight_stream');
-	});
-	$("#stream3_btn").mouseout("click", function() {
-		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream3").addClass('highlight_stream');
-	  	
 	$("#button1").click(function(){
 		$("#button1").hide("fast");
+	});
+	$("#stream1_btn").click(function(){
+		$(".stream1").fadeOut(1000, 0.4);
+		$(".stream1").fadeIn(1000, 1);
 	});
 }); 
